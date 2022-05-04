31009d093c85a7c39d19ae82825136e4ac535f77
TanatornZ <80368758+TanatornZ@users.noreply.github.com>
2022-05-04 10:55:02 +0700

    add Json

---
+++ Json/newArrivals.ts
@@ -0,0 +1,51 @@
+export const newArrivals = [
+  {
+    id: 1,
+    name: "Haunted House",
+    category: "Accessory",
+    price: 14.99,
+    discount: 29.99,
+    img : 'new1-img'
+  },
+  {
+    id: 2,
+    name: "Halloween Candle",
+    category: "Accessory",
+    price: 11.99,
+    discount: 21.99,
+    img : 'new2-img'
+  },
+  {
+    id: 3,
+    name: "Witch Hat",
+    category: "Accessory",
+    price: 4.99,
+    discount: 9.99,
+    img : 'new3-img'
+  },
+  {
+    id: 4,
+    name: "Rip",
+    category: "Accessory",
+    price: 14.99,
+    discount: 29.99,
+    img : 'new4-img'
+  },
+  {
+    id: 5,
+    name: "Terrifying Crystal Ball",
+    category: "Accessory",
+    price: 5.99,
+    discount: 12.99,
+    img : 'new5-img'
+  },
+  {
+    id: 6,
+    name: "Witch Broom",
+    category: "Accessory",
+    price: 7.99,
+    discount: 9.99,
+    img : 'new6-img'
+  },
+  
+];
---
+++ Json/trickOrTreat.ts
@@ -0,0 +1,8 @@
+export const trickOrTreat = [
+  { id: 1, name: "Toffee", description: "Candy", price: "11.99", img: "trick-treat1-img.png" },
+  { id: 2, name: "Bone", description: "Accessory", price: "8.99", img: "trick-treat2-img.png" },
+  { id: 3, name: "Scarecrow", description: "Accessory", price: "15.99", img: "trick-treat3-img.png" },
+  { id: 4, name: "Candy Cane", description: "Candy", price: "7.99", img: "trick-treat4-img.png" },
+  { id: 5, name: "Pumpkin", description: "Candy", price: "19.99", img: "trick-treat5-img.png" },
+  { id: 6, name: "Ghost", description: "Accessory", price: "17.99", img: "trick-treat6-img.png" },
+];
